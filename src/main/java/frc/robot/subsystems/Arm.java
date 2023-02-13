8e50a52eaf40131d5dc2e2a5a9020c1cb95a4640
Ryan Brady <rbrady.business@gmail.com>
2023-02-13 17:48:09 -0500

    shuffleboard graph

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,10 +4,13 @@
 
 package frc.robot.subsystems;
 
+import java.util.LinkedList;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.XboxController;
 //import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 //import edu.wpi.first.wpilibj.motorcontrol.Victor;
@@ -19,7 +22,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm extends SubsystemBase {
 
-
+  private static final String kGraphTitle = "Encoder Position";
+  private static final int kMaxDataPoints = 100;
 
   private static final CANSparkMax armMotor = new CANSparkMax(Constants.ARM_MOTOR, MotorType.kBrushless);
 
@@ -27,9 +31,21 @@ public class Arm extends SubsystemBase {
 
   private static RelativeEncoder m_Encoder = armMotor.getEncoder();
   private static boolean aPressed = false;
-  private static boolean bPressed = false;
+  private static boolean bPressed = false; 
 
-
+//   public void graphEncoderPosition() {
+//     ShuffleboardTab tab = Shuffleboard.getTab("Arm Subsystem");
+//     NetworkTableEntry graph = tab.add(kGraphTitle, new LinkedList<Double>())
+//         .withSize(2, 2)
+//         .withPosition(0, 0)
+//         .getEntry();
+//     LinkedList<Double> data = (LinkedList<Double>) graph.getValue();
+//     data.addFirst(m_Encoder.getPosition());
+//     while (data.size() > kMaxDataPoints) {
+//         data.removeLast();
+//     }
+//     graph.setValue(data);
+// }
 
   public static void PIDMoveArm()
   {
@@ -40,6 +56,12 @@ public class Arm extends SubsystemBase {
     armMotor.getPIDController().setD(Constants.PID_D);
     armMotor.getPIDController().setFF(Constants.PID_FF);
     armMotor.getPIDController().setIZone(Constants.PID_I_ZONE);
+
+    
+    double encoderPosition = m_Encoder.getPosition();
+    SmartDashboard.putNumber("Encoder Position", encoderPosition);
+
+
 
     System.out.println("A = " + driverController.getAButton() + " B = " + driverController.getBButton() + " Encoder Value = " + m_Encoder.getPosition() + " Motor Temp " + armMotor.getMotorTemperature());
  
@@ -59,10 +81,10 @@ public class Arm extends SubsystemBase {
     {
       armMotor.set(0.0);
     }
-
-
-
   }
+
+  
+  
 
 
   public Arm() {
